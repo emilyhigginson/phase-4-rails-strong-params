065e4be6d65a432aafdbaf85f4859dbadbd1a843
Emily <emilyhigginson14@gmail.com>
2022-03-14 11:35:19 -0400

    commit tree 2549bdca86ac8a3f37b868b9a2827a0ee307d831

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -5,10 +5,9 @@ class BirdsController < ApplicationController
     birds = Bird.all
     render json: birds
   end
-
-  # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    # bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
 
@@ -20,6 +19,12 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  private
+
+  def bird_params
+    params.permit(:name, :species)
   end
 
 end
